d8ad1a89201482d037ec94ad7df2eef1fa98c3cd
Maxim <334890@gmail.com>
2022-12-20 13:27:26 +1000

    Add function getComputerChoice

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("testing");
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3) + 1;
+  switch (choice) {
+    case 1:
+      return "Rock";
+      break;
+    case 2:
+      return "Paper";
+      break;
+    case 3:
+      return "Scissors";
+      break;
+
+    default:
+      break;
+  }
+}
