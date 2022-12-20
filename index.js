d372bc033af20d387572e0bbf08f1e33c6e10c58
Maxim <334890@gmail.com>
2022-12-20 15:03:17 +1000

    Add game function

--- index.js
+++ index.js
@@ -91,4 +91,30 @@ function getUserChoice() {
   return userChoice;
 }
 
-function game() {}
+function game() {
+  let score = {
+    computer: 0,
+    user: 0,
+  };
+  for (let i = 1; i <= 5; i++) {
+    let roundResult = playRound(getUserChoice(), getComputerChoice());
+    //console.log(roundResult);
+    //roundResult = roundResult.toLowerCase()
+    if (roundResult.includes("win")) {
+      score.user += 1;
+    } else if (roundResult.includes("lose")) {
+      score.computer += 1;
+    } else if (roundResult.includes("Draw")) {
+    } else {
+      console.log("wtf?");
+    }
+    console.log(`${score.user}:${score.computer} ${roundResult}`);
+  }
+  console.log(`P${score.user} : C${score.computer}`);
+  //call getUserChoice
+  //call playRound
+  //display result
+  //update score
+
+  //display match score
+}
